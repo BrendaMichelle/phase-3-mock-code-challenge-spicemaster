f76d162f45b341f42ceb757a5ed3fc3f090f1fa3
Michelle Rios <brendamichelle995@gmail.com>
2021-02-10 14:09:59 -0500

    finish core deliverables

--- src/index.js
+++ src/index.js
@@ -20,3 +20,51 @@ fetch('http://localhost:3000/spiceblends/1')
             ingredientsUl.append(li)
         })
     })
+
+
+// Update the title of the spice blend on the page when the #update-form 
+// is submitted, and still see that change when reloading the page 
+// (the new title should be persisted on the server).
+
+const updateForm = document.querySelector('#update-form')
+updateForm.addEventListener('submit', event => {
+    event.preventDefault()
+    // console.log(event.target.title.value) // using name attribute on the input element
+    // console.log(event.target[0].value)
+    const titleInput = document.querySelector('#spiceblend-title').value
+    // optimistic
+    // const titleH2 = document.querySelector('h2.title')
+    // titleH2.textContent = titleInput
+
+    fetch('http://localhost:3000/spiceblends/1', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: titleInput })
+    })
+        .then(response => response.json())
+        .then(updatedSpiceBlend => {
+            // pessimistic
+            const titleH2 = document.querySelector('h2.title')
+            titleH2.textContent = updatedSpiceBlend.title
+        })
+})
+
+
+
+// Add a new ingredient to the spice blend when the #ingredient-form 
+// is submitted. The new ingredient should be displayed on the page 
+// (no persistence needed for now).
+const ingredientForm = document.querySelector('#ingredient-form')
+ingredientForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const nameInput = event.target.name.value
+    console.log(nameInput)
+
+    const li = document.createElement('li')
+    li.textContent = nameInput
+
+    const ingredientUl = document.querySelector('.ingredients-list')
+    ingredientUl.append(li)
+})
