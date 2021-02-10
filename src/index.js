958d7fd1fbe10574e741d40e6957aa52fc9b3670
Michelle Rios <brendamichelle995@gmail.com>
2021-02-10 15:10:30 -0500

    all deliverables complete; before refactor

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
 // write your code here
+const spiceBlendUrl = 'http://localhost:3000/spiceblends'
+const spiceImages = document.querySelector('#spice-images')
+let currentSpiceBlend = 1
 
 // See the first spice blend (the spice blend with an ID of 1), 
 // including its title, image, and list of ingredients, 
 // when the page loads.
-fetch('http://localhost:3000/spiceblends/1')
-    .then(response => response.json())
-    .then(spiceBlendObject => {
-        const spiceBlendDetailDiv = document.querySelector('#spice-blend-detail')
-        const spiceDetailImg = spiceBlendDetailDiv.querySelector('img')
-        const titleH2 = spiceBlendDetailDiv.querySelector('h2')
-        const ingredientsUl = spiceBlendDetailDiv.querySelector('ul')
+function displayHighlightedSpice() {
+    fetch(`${spiceBlendUrl}/${currentSpiceBlend}`)
+        .then(response => response.json())
+        .then(spiceBlendObject => {
+            const spiceBlendDetailDiv = document.querySelector('#spice-blend-detail')
+            const spiceDetailImg = spiceBlendDetailDiv.querySelector('img')
+            const titleH2 = spiceBlendDetailDiv.querySelector('h2')
+            const ingredientsUl = spiceBlendDetailDiv.querySelector('ul')
 
-        spiceDetailImg.src = spiceBlendObject.image
-        titleH2.textContent = spiceBlendObject.title
+            spiceDetailImg.src = spiceBlendObject.image
+            titleH2.textContent = spiceBlendObject.title
 
-        spiceBlendObject.ingredients.forEach(ingredient => {
-            const li = document.createElement('li')
-            li.textContent = ingredient.name
-            ingredientsUl.append(li)
+            ingredientsUl.innerHTML = ''
+
+            // const lis = ingredientsUl.querySelectorAll('li')
+            // lis.forEach(li => li.remove())
+
+            spiceBlendObject.ingredients.forEach(ingredient => {
+                const li = document.createElement('li')
+                li.textContent = ingredient.name
+                ingredientsUl.append(li)
+            })
         })
-    })
+}
+
 
 
 // Update the title of the spice blend on the page when the #update-form 
@@ -36,7 +47,7 @@ updateForm.addEventListener('submit', event => {
     // const titleH2 = document.querySelector('h2.title')
     // titleH2.textContent = titleInput
 
-    fetch('http://localhost:3000/spiceblends/1', {
+    fetch(`http://localhost:3000/spiceblends/${currentSpiceBlend}`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json'
@@ -56,6 +67,10 @@ updateForm.addEventListener('submit', event => {
 // Add a new ingredient to the spice blend when the #ingredient-form 
 // is submitted. The new ingredient should be displayed on the page 
 // (no persistence needed for now).
+// Persist new ingredients to the server when the #ingredient-form is submitted. 
+// Make sure to follow the format listed above to make a POST request 
+// to /ingredients. In the body of the request, the spiceblendId will 
+// need to be a number (not a string).
 const ingredientForm = document.querySelector('#ingredient-form')
 ingredientForm.addEventListener('submit', event => {
     event.preventDefault()
@@ -67,4 +82,61 @@ ingredientForm.addEventListener('submit', event => {
 
     const ingredientUl = document.querySelector('.ingredients-list')
     ingredientUl.append(li)
+
+
+    fetch('http://localhost:3000/ingredients', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: nameInput, spiceblendId: parseInt(currentSpiceBlend) })
+    })
+        .then(response => response.json())
+        .then(newIngredient => console.log(newIngredient))
 })
+
+
+// See all spice blend images in the #spice-images div when the page loads. 
+// Request the data from the server to get all the spice blends. 
+// Then, display the image for each of the spice blends using an img tag 
+// inside the #spice-images div.
+
+fetch(spiceBlendUrl)
+    .then(response => response.json())
+    .then(spiceBlendsArr => {
+        spiceBlendsArr.forEach(spiceBlend => {
+            const img = document.createElement('img')
+            img.src = spiceBlend.image
+            img.dataset.id = spiceBlend.id
+            spiceImages.append(img)
+        })
+    })
+
+
+
+// Click on an image from the #spice-images div and see all the info 
+// about that spice blend displayed inside the #spice-blend-detail div. 
+// You will need to make another GET request with the spice blend's ID to 
+// get the information about the spice blend that was clicked.
+spiceImages.addEventListener('click', event => {
+    // event.target.tagName === "IMG"
+    if (event.target.matches('img')) {
+        console.log(event.target)
+        currentSpiceBlend = event.target.dataset.id
+        displayHighlightedSpice()
+    }
+})
+
+
+
+
+
+
+/* Initial Render */
+displayHighlightedSpice()
+
+
+
+
+
+
